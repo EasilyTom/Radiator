483e78e8ead5d618c01d5d423a20c8ea99b593ce
Furri777 <tomwilkinson7@me.com>
2023-05-08 17:08:04 +0100

    Renamed Model.slx to Radiator.slx Model now directly references heat_exchanger_properties.m to load values, rather than having to run the file on startup. heat_exchanger_properties.m no longer runs on startup, and clear all has been removed as it isn't needed

--- heat_exchanger_properties.m
+++ heat_exchanger_properties.m
@@ -1,6 +1,6 @@
 %https://uk.mathworks.com/help/hydro/ug/modeling-heat-exchangers.html
-clear all;
 
+% Radiator specs
 radiator_L = 0.6; % [m]
 radiator_W = 0.015; % [m]
 radiator_H = 0.2; % [m]
@@ -10,6 +10,7 @@ fin_spacing = 0.002; % [m]
 wall_thickness = 1e-4; % [m]
 wall_conductivity = 240; % [W/(m*K)]
 liquid_pipe_D = 0.019; % [m]
+% Derived specs
 tube_H_internal = tube_H - 2*wall_thickness; % [m]
 tube_W_internal = radiator_W - 2*wall_thickness; % [m]
 gap_H = (radiator_H - tubes_N * tube_H) / (tubes_N - 1); % [m]
@@ -20,6 +21,7 @@ air_area_fins = 2 * fins_N * radiator_W * gap_H; % [m^2]
 thermal_resistance_primary = wall_thickness / air_area_primary / wall_conductivity; % [K/kW]
 liquid_pipe_A = pi * liquid_pipe_D^2 / 4; % [m^2]
 
-coolant_hot_temp = 273 + 77; % 350K
+% Other values
+coolant_hot_temp = 350; % [K]
 coolant_flow_rate = 0.1; % [kg/s]
 p_diff_across_radiator = 2500; % [Pa]
