66a03089eb587388b1dc4848127ad06f138a46a9
Furri777 <tomwilkinson7@me.com>
2023-10-09 11:07:48 +0100

    Add internal thermal mass to components
    Inverter and Motor are now modelled as having a thermal mass, so the temperature of the components can be viewed too

--- heat_exchanger_properties.m
+++ heat_exchanger_properties.m
@@ -3,13 +3,13 @@
 % Radiator specs
 radiator_L = 0.6; % [m]
 radiator_W = 0.015; % [m]
-radiator_H = 0.2; % [m]
-tubes_N = 25;
+radiator_H = 0.5; % [m]
+tubes_N = 50;
 tube_H = 0.0015; % [m]
 fin_spacing = 0.002; % [m]
 wall_thickness = 1e-4; % [m]
 wall_conductivity = 240; % [W/(m*K)]
-liquid_pipe_D = 0.019; % [m]
+liquid_pipe_D = 0.01; % [m]
 % Derived specs
 tube_H_internal = tube_H - 2*wall_thickness; % [m]
 tube_W_internal = radiator_W - 2*wall_thickness; % [m]
@@ -23,5 +23,17 @@ liquid_pipe_A = pi * liquid_pipe_D^2 / 4; % [m^2]
 
 % Other values
 coolant_hot_temp = 350; % [K]
-coolant_flow_rate = 0.1; % [kg/s]
-p_diff_across_radiator = 2500; % [Pa]
+coolant_flow_rate = 15 /60; % [L/min]
+p_diff_across_radiator = 25000; % [Pa]
+
+% Motor specs
+motor_min_efficiency = 0.9;
+motor_peak_power = 86000; % [W]
+motor_max_heat = 1800;%motor_peak_power * (1 - motor_min_efficiency); % [W]
+
+% Inverter specs
+inverter_efficiency = 0.95;
+inverter_max_heat = (motor_peak_power/inverter_efficiency) * (1 - inverter_efficiency); % [W]
+
+% Total heat
+total_max_heat = motor_max_heat + inverter_max_heat; % [W]
